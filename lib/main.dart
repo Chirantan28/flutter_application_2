b0c557efb1b495671aa59d730d6ae1c715437755
Chirantan28 <chirantanofficial28@gmail.com>
2024-12-20 11:07:06 +0530

    animation for Deliverlocation

--- lib/main.dart
+++ lib/main.dart
@@ -366,7 +366,158 @@ class _MainAppPageState extends State<MainAppPage> {
   }
 }
 
-class CartPage extends StatelessWidget {
+// class CartPage extends StatelessWidget {
+//   final TextEditingController nameController = TextEditingController();
+//   final TextEditingController classNumberController = TextEditingController();
+//   String selectedBlock = 'ESB';
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Your Cart'),
+//       ),
+//       body: cartState.items.isEmpty
+//           ? Center(child: Text('Your cart is empty!'))
+//           : Column(
+//               children: [
+//                 Expanded(
+//                   child: ListView.builder(
+//                     itemCount: cartState.items.length,
+//                     itemBuilder: (context, index) {
+//                       final item = cartState.items[index];
+//                       return ListTile(
+//                         title: Text(item.name),
+//                         subtitle: Text('₹${item.price} x ${item.quantity}'),
+//                         trailing: IconButton(
+//                           icon: Icon(Icons.delete),
+//                           onPressed: () {
+//                             cartState.removeItem(item.name);
+//                             (context as Element).markNeedsBuild();
+//                           },
+//                         ),
+//                       );
+//                     },
+//                   ),
+//                 ),
+//                 Divider(),
+//                 ListTile(
+//                   title: Text('Subtotal'),
+//                   trailing: Text('₹${cartState.subtotal}'),
+//                 ),
+//                 ListTile(
+//                   title: Text('Stud Charge'),
+//                   trailing: Text('₹${cartState.studCharge}'),
+//                 ),
+//                 ListTile(
+//                   title: Text('Total'),
+//                   trailing: Text('₹${cartState.total}'),
+//                 ),
+//                 Padding(
+//                   padding: const EdgeInsets.all(8.0),
+//                   child: Column(
+//                     children: [
+//                       TextField(
+//                         controller: nameController,
+//                         decoration: InputDecoration(
+//                           labelText: 'Name',
+//                           border: OutlineInputBorder(
+//                             borderRadius: BorderRadius.circular(12),
+//                           ),
+//                         ),
+//                       ),
+//                       SizedBox(height: 10),
+//                       DropdownButtonFormField<String>(
+//                         value: selectedBlock,
+//                         decoration: InputDecoration(
+//                           labelText: 'Block Name',
+//                           border: OutlineInputBorder(
+//                             borderRadius: BorderRadius.circular(12),
+//                           ),
+//                         ),
+//                         items: [
+//                           'ESB',
+//                           'DES',
+//                           'LHC',
+//                           'ARCHITECTURE',
+//                           'CRD',
+//                           'WWORKSHOP'
+//                         ]
+//                             .map((block) => DropdownMenuItem(
+//                                   child: Text(block),
+//                                   value: block,
+//                                 ))
+//                             .toList(),
+//                         onChanged: (value) {
+//                           selectedBlock = value!;
+//                         },
+//                       ),
+//                       SizedBox(height: 10),
+//                       TextField(
+//                         controller: classNumberController,
+//                         decoration: InputDecoration(
+//                           labelText: 'Class Number',
+//                           border: OutlineInputBorder(
+//                             borderRadius: BorderRadius.circular(12),
+//                           ),
+//                         ),
+//                       ),
+//                       SizedBox(height: 10),
+//                       ElevatedButton(
+//                         onPressed: () {
+//                           // Proceed to payment or checkout
+//                         },
+//                         child: Text('Proceed to Payment'),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               ],
+//             ),
+//     );
+//   }
+// }
+class CartPage extends StatefulWidget {
+  @override
+  _CartPageState createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage>
+    with SingleTickerProviderStateMixin {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController classNumberController = TextEditingController();
+  String selectedBlock = 'ESB';
+  bool showDeliveryLocation = false;
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    );
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeInOut);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _toggleDeliveryLocation() {
+    setState(() {
+      showDeliveryLocation = !showDeliveryLocation;
+      if (showDeliveryLocation) {
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -412,10 +563,71 @@ class CartPage extends StatelessWidget {
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: ElevatedButton(
-                    onPressed: () {
-                      // Proceed to payment or checkout
-                    },
-                    child: Text('Proceed to Payment'),
+                    onPressed: _toggleDeliveryLocation,
+                    child: Text('Confirm'),
+                  ),
+                ),
+                SizeTransition(
+                  sizeFactor: _animation,
+                  axisAlignment: -1.0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        TextField(
+                          controller: nameController,
+                          decoration: InputDecoration(
+                            labelText: 'Name',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        DropdownButtonFormField<String>(
+                          value: selectedBlock,
+                          decoration: InputDecoration(
+                            labelText: 'Block Name',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                          items: [
+                            'ESB',
+                            'DES',
+                            'LHC',
+                            'ARCHITECTURE',
+                            'CRD',
+                            'WWORKSHOP'
+                          ]
+                              .map((block) => DropdownMenuItem(
+                                    child: Text(block),
+                                    value: block,
+                                  ))
+                              .toList(),
+                          onChanged: (value) {
+                            selectedBlock = value!;
+                          },
+                        ),
+                        SizedBox(height: 10),
+                        TextField(
+                          controller: classNumberController,
+                          decoration: InputDecoration(
+                            labelText: 'Class Number',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Proceed to payment or checkout
+                          },
+                          child: Text('Proceed to Payment'),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
@@ -423,7 +635,6 @@ class CartPage extends StatelessWidget {
     );
   }
 }
-
 // Orderer Page
 
 class OrdererPage extends StatelessWidget {
